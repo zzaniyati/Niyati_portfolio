6b55a8cf99541c6246707837d9da4fc06530cff8
Niyati Oza <143085652+zzaniyati@users.noreply.github.com>
2023-11-18 15:30:35 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const navbarMenu = document.querySelector('.navbar-menu');
+    navbarMenu.style.display = navbarMenu.style.display === 'flex' ? 'none' : 'flex';
+  }
+  
